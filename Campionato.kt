1df37367eb5287ccad01621d55ec516cebc2663c
unknown <you@example.com>
2022-02-23 21:57:44 -0300

    aula11

---
+++ Campionato.kt
@@ -0,0 +1,14 @@
+class Campionato(
+    val nome: String,
+    val ano: Short
+) {
+    private var clubes = mutableListOf<Clube>()
+
+    fun cadastraNovoClube(clube: Clube): Boolean = clubes.add(clube)
+
+    fun listarTodosOsClubes() = clubes
+
+    fun listarClubesPorSerie(serie: Serie) = clubes.filter{ c -> c.serie == serie }
+
+    fun buscarClubePeloNome(nome: String) = clubes.first{ c -> c.nome.equals(nome,true) }
+}
---
+++ Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+//    val gauchao = Campionato(nome = "Gauchão", ano = 2022)
+//
+//    println("N° de clubes ${gauchao.listarTodosOsClubes().size}")
+//
+//    gauchao.cadastraNovoClube(
+//        Clube(
+//            nome = "Grêmio",
+//            serie = Serie.A
+//        )
+//    )
+//    println("N° de clubes ${gauchao.listarTodosOsClubes().size}")
+//
+//    val times = gauchao.listarTodosOsClubes()
+//    val contagem = times.size - 1
+//    for(indice in 0..contagem){
+//        println("Nome: ${times[indice].nome}\nSérie: ${times[indice].serie}")
+//    }
+
+    for(cont in 1..10 step 2){
+        println(cont)
+    }
+}
